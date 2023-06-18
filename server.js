cd6205f91a7fd076b605dd68c39d681d9f07ae38
Christine Wong <jumiryoo@gmail.com>
2023-06-17 21:26:48 -0700

    new and edit pages added

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const morgan = require("morgan")
 const app = express();
 const PORT = process.env.PORT; 
+const methodOverride = require("method-override")
 
 const pokemons = require('./models/pokemon')
 
 app.use(morgan("dev"))
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride("_method"))
 
 app.get("/", (req, res) => {
     res.send(`welcome to pokedex`)
@@ -22,7 +25,27 @@ app.get("/pokemon/:id", (req, res) => {
     const id = req.params.id
     const pokemon = pokemons[id]
     res.render("show.ejs", ({pokemon}))
+    console.log(pokemon)
 })
+
+// NEW - GET 
+app.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+// CREATE - POST
+app.post("/pokemon", (req, res) => {
+   pokemons.push(req.body)
+   res.redirect("/pokemon")
+})
+
+// EDIT
+app.get("/pokemon/:id/edit", (req, res) => {
+    const id = req.params.id
+    const pokemon = pokemons[id]
+    res.render("edit.ejs", {pokemon, id})
+})
+
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
