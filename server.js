3b173013d2e72ad0b30034b207abec95f95be609
Christine Wong <jumiryoo@gmail.com>
2023-06-18 22:18:49 -0700

    got all the routes to work

--- server.js
+++ server.js
@@ -1,50 +1,70 @@
-require('dotenv').config()
+// IMPORT DEPENDENCIES & SETUP
+require('dotenv').config();
 const express = require('express');
-const morgan = require("morgan")
+const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT; 
 const methodOverride = require("method-override")
 
-const pokemons = require('./models/pokemon')
+const pokemons = require("./models/pokemon")
 
+// MIDDLEWARE
 app.use(morgan("dev"))
+app.use(express.static("public"))
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride("_method"))
 
-app.get("/", (req, res) => {
-    res.send(`welcome to pokedex`)
-})
 
-// INDEX
+// ROUTES
+
+// INDEX - GET - SHOWS ALL POKEMONS
 app.get("/pokemon", (req, res) => {
     res.render("index.ejs", {pokemons})
 })
 
-// SHOW
+// NEW - GET - RENDER FORM TO CREATE A POKEMON
+app.get("/pokemon/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+// DESTROY - DELETE - DELETE A FRUIT
+app.delete("/pokemon/:id", (req, res) => {
+    const id = req.params.id
+    pokemons.splice(id ,1)
+    res.redirect("/pokemon")
+})
+
+// UPDATE - PUT - UPDATE A POKEMON
+app.put("/pokemon/:id", (req, res) => {
+    const id = req.params.id
+    pokemons[id] = req.body
+    res.redirect("/pokemon")
+})
+
+// CREATE - POST - CREATE A POKEMON
+app.post("/pokemon", (req, res) => {
+    pokemons.push(req.body)
+    res.redirect("/pokemon")
+ })
+
+// EDIT - GET - RENDER FORM TO UPDATE A POKEMON
+app.get("/pokemon/:id/edit", (req, res) => {
+    const id = req.params.id
+    const pokemon = pokemons[id]
+    res.render("edit.ejs", {pokemon, id})
+})
+ 
+
+// SHOW - GET - SHOWS ONE POKEMON 
 app.get("/pokemon/:id", (req, res) => {
     const id = req.params.id
     const pokemon = pokemons[id]
     res.render("show.ejs", ({pokemon, id}))
 })
 
-// NEW - GET 
-app.get("/new", (req, res) => {
-    res.render("new.ejs")
-})
 
-// CREATE - POST
-app.post("/pokemon", (req, res) => {
-   pokemons.push(req.body)
-   res.redirect("/pokemon")
-})
 
-// EDIT
-app.get("/pokemon/:id/edit", (req, res) => {
-    const id = req.params.id
-    const pokemon = pokemons[id]
-    res.render("edit.ejs", {pokemon, id})
-})
-
+// LISTENER
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
