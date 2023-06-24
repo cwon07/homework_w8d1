acd1260727fae393693d76671d6c4e72f9a90a1a
Christine Wong <jumiryoo@gmail.com>
2023-06-23 23:27:55 -0700

    all the CRUD codes are working

---
+++ controllers/pokemons.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const pokemons = require('../models/pokemon')
+
+// ROUTER
+const router = express.Router()
+
+// ROUTES
+
+// INDEX - GET - SHOWS ALL POKEMONS
+router.get("/", (req, res) => {
+    res.render("index.ejs", {pokemons})
+})
+
+// NEW - GET - RENDER FORM TO CREATE A POKEMON
+router.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+// DESTROY - DELETE - DELETE A POKEMON
+router.delete("/:id", (req, res) => {
+    const id = req.params.id
+    pokemons.splice(id ,1)
+    res.redirect("/pokemon")
+})
+
+// UPDATE - PUT - UPDATE A POKEMON
+router.put("/:id", (req, res) => {
+    const id = req.params.id
+    pokemons[id] = {
+        name: req.body.name,
+        img: req.body.image,
+        type: [
+            req.body.type
+        ],
+        stats: {
+            hp: req.body.hp,
+            attack: req.body.attack,
+            defense: req.body.defense
+        }
+    }
+    res.redirect("/pokemon")
+})
+
+// CREATE - POST - CREATE A POKEMON
+router.post("/", (req, res) => {
+    let newPokemon = {
+        name: req.body.name,
+        img: req.body.image,
+        type: [
+            req.body.type
+        ],
+        stats: {
+            hp: req.body.hp,
+            attack: req.body.attack,
+            defense: req.body.defense
+        }
+    }
+    pokemons.push(newPokemon)
+    res.redirect("/pokemon")
+ })
+
+// EDIT - GET - RENDER FORM TO UPDATE A POKEMON
+router.get("/:id/edit", (req, res) => {
+    const id = req.params.id
+    const pokemon = pokemons[id]
+    res.render("edit.ejs", ({pokemon, id}))
+})
+ 
+
+// SHOW - GET - SHOWS ONE POKEMON 
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+    const pokemon = pokemons[id]
+    console.log(pokemon)
+    res.render("show.ejs", ({pokemon, id}))
+})
+
+// EXPORT ROUTER
+module.exports = router
--- server.js
+++ server.js
@@ -5,68 +5,14 @@ const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT; 
 const methodOverride = require("method-override")
-
-const pokemons = require("./models/pokemon")
-const retrievePokemonData = () => {
-    return []
-}
+const pokemonsRouter = require("./controllers/pokemons")
 
 // MIDDLEWARE
 app.use(morgan("dev"))
-app.use(express.static("public"))
+app.use("../public", express.static("public"))
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride("_method"))
-
-
-// ROUTES
-
-// INDEX - GET - SHOWS ALL POKEMONS
-app.get("/pokemon", (req, res) => {
-    const updatedPokemons = retrievePokemonData();
-    res.render("index.ejs", {pokemons, updatedPokemons})
-})
-
-// NEW - GET - RENDER FORM TO CREATE A POKEMON
-app.get("/pokemon/new", (req, res) => {
-    res.render("new.ejs")
-})
-
-// DESTROY - DELETE - DELETE A FRUIT
-app.delete("/pokemon/:id", (req, res) => {
-    const id = req.params.id
-    pokemons.splice(id ,1)
-    res.redirect("/pokemon")
-})
-
-// UPDATE - PUT - UPDATE A POKEMON
-app.put("/pokemon/:id", (req, res) => {
-    const id = req.params.id
-    pokemons[id] = req.body
-    res.redirect("/pokemon")
-})
-
-// CREATE - POST - CREATE A POKEMON
-app.post("/pokemon", (req, res) => {
-    pokemons.push(req.body)
-    res.redirect("/pokemon")
- })
-
-// EDIT - GET - RENDER FORM TO UPDATE A POKEMON
-app.get("/pokemon/:id/edit", (req, res) => {
-    const id = req.params.id
-    const pokemon = pokemons[id]
-    const updatedPokemons = retrievePokemonData();
-    res.render("edit.ejs", {pokemon, id, updatedPokemons})
-})
- 
-
-// SHOW - GET - SHOWS ONE POKEMON 
-app.get("/pokemon/:id", (req, res) => {
-    const id = req.params.id
-    const pokemon = pokemons[id]
-    res.render("show.ejs", ({pokemon, id}))
-})
-
+app.use("/pokemon", pokemonsRouter)
 
 
 // LISTENER
